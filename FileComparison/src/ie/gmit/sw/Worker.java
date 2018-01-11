efa738b6811992c158395aac0a95bab037486819
ZqhKHP <G00336837@gmit.ie>
2018-01-11 02:29:56 +0000

    adding author

--- FileComparison/src/ie/gmit/sw/Worker.java
+++ FileComparison/src/ie/gmit/sw/Worker.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
-//implements callable interface to allow multithreading of documents
+
 /*
- * Callable is same as Runnable but it can return any type of Object if we want to get a result or status from work (callable).
+ * Implements callable interface to allow multithreading of documents
+ * Callable is same as Runnable but it can return any type of Object if we
+ *  want to get a result or status from work (callable).
+ *  Create Thread constructor to take 3 parameters, int, File and int.
  */
 public class Worker implements Callable<Document> {
 	private int docId;
@@ -20,6 +23,13 @@ public class Worker implements Callable<Document> {
 	}
 	
 	//adapted from https://dzone.com/articles/java-callable-future-understanding
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.concurrent.Callable#call()
+	 * Call method responsible for returning a Document object.
+	 * Gives application the ability for multi - threading.
+	 * {@author Gary Connelly}
+	 */
 	
 	//overriding implement methods
 	@Override
