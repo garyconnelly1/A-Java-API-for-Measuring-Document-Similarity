db3630b7c44e1790f25fbf684186e2f4a3b7d85c
ZqhKHP <G00336837@gmit.ie>
2018-01-11 02:19:34 +0000

    commenting and formatting

--- FileComparison/src/ie/gmit/sw/ShingleParser.java
+++ FileComparison/src/ie/gmit/sw/ShingleParser.java
@@ -10,6 +10,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+/*
+ * Class that is constructed with only 1 int parameter, shingleSize(4).
+ * Uses an InputStream to parse the words in the document to shingles.
+ *
+ */
+
+
 public class ShingleParser {
 	
 	private int shingleSize;
@@ -17,6 +24,18 @@ public class ShingleParser {
 	public ShingleParser(int shingleSize) {
 		this.shingleSize = shingleSize;
 	}
+	
+	/*
+	 * Parse method just takes an input stream as a parameter.
+	 * Creates a hashSet called shingles. Calles the method parseWords.
+	 * Creates a StringBuilder object and an Iterator object.
+	 * If counter == 3 then add the words in groups of 4(shingleSize) 
+	 * to the shingles set after getting the hashcode of that group of words
+	 * reset counter to 0 and make a new StringBuilder.
+	 * Otherwise add the next iterated string to the StringBuilder.
+	 * Increment counter.
+	 * 
+	 */
 	
 	public Set<Integer> parse(InputStream stream) throws IOException {
 		Set<Integer> shingles = new LinkedHashSet<Integer>();
@@ -26,7 +45,7 @@ public class ShingleParser {
 		Iterator<String> iter = buffer.iterator();
 		while(iter.hasNext()) {
 			if (counter == shingleSize - 1) {
-				//add the words in groups of 4(shingleSize) to the shingles set after getting the hashcode of that group of words
+				
 				shingles.add(sb.toString().toUpperCase().hashCode());
 				counter = 0;
 				sb = new StringBuilder();
@@ -40,6 +59,13 @@ public class ShingleParser {
 		return shingles;
 	}
 	
+	/*
+	 * Method for how the words should be parsed.
+	 * Just takes an InputStream as a parameter.
+	 * Creates a BufferedReader object with the input stream as a parameter.
+	 * Split the lines at the space.
+	 * Adds the words and index [i] to the buffer and returns the buffer.
+	 */
 	
 	private List<String> parseWords(InputStream stream) throws IOException {
 		List<String> buffer = new LinkedList<String>();
@@ -47,7 +73,7 @@ public class ShingleParser {
 		String line = null;
 		//while there is a next line
 		while ((line = br.readLine()) != null) {
-			//split the lines at the space
+			//
 			String[] words = line.split(" ");
 			for(int i = 0; i < words.length; i++) {
 				buffer.add(words[i]);
