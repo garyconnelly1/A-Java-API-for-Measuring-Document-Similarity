da180125a91f6d1291aee9ca982fe2b83dbe0e41
ZqhKHP <G00336837@gmit.ie>
2018-01-11 02:17:25 +0000

    commenting and formatting

--- FileComparison/src/ie/gmit/sw/Document.java
+++ FileComparison/src/ie/gmit/sw/Document.java
@@ -1,3 +1,7 @@
+/*
+ * Simple class used to create a document object so that
+ * it can be compared to another document object later on in the program.
+ */
 package ie.gmit.sw;
 
 import java.util.Set;
@@ -6,11 +10,19 @@ public class Document {
 	private int id;
 	private Set<Integer> shingles;
 
+	/*
+	 * constructor consisting of an integer and a set of shingles.
+	 */
+	
+	
 	public Document(int id, Set<Integer> shingles) {
 		this.id = id;
 		this.shingles = shingles;
 	}
 
+	/*
+	 * Simple getter methods with a toString method for formatting.
+	 */
 	public int getId() {
 		return id;
 	}
